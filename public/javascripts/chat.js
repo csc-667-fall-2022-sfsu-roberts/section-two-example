a336b399bd1627fca7ab361f0f050c11e49b2bcb
John Roberts <roberts.john@gmail.com>
2022-12-01 18:16:27 -0800

    sockets

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -2,6 +2,7 @@ const socket = io();
 
 const messages = document.querySelector("#messages");
 const input = document.querySelector("#message");
+const messageTemplate = document.querySelector("#message-content");
 
 input.addEventListener("keydown", (event) => {
   if (event.keyCode === 13) {
@@ -17,4 +18,12 @@ input.addEventListener("keydown", (event) => {
 
 socket.on("chat:0", ({ username, message, timestamp }) => {
   console.log({ username, message, timestamp });
+
+  const content = messageTemplate.content.cloneNode(true);
+
+  content.querySelector(".username").innerText = username;
+  content.querySelector(".message").innerText = message;
+  content.querySelector(".timestamp").innerText = timestamp;
+
+  messages.appendChild(content);
 });
